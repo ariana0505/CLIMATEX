6252e9c109a599eee20ac9f3a34aeeaad5009b9c
ariana0505 <penaariana075@gmail.com>
2025-01-26 12:54:56 -0500

    INICIANDO

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+//accb6ad0ec1f2649f67009bcf6830490 
+function App() {
+  const [city, setCity] = useState('')
+  const [weatherData, setWeatherData] = useState(null)
+  return (
+    <>
+      
+    </>
+  )
+}
+
+export default App
